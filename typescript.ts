a7998c8e166edfb9e0d1459b24fb2b919d89c28a
nemong <105049274+NEMONG70@users.noreply.github.com>
2023-01-20 20:07:12 +0900

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -1,7 +1,7 @@
 function random(Arr: Array<{ value: any; per: number }>): any {
   if (Arr.length === 1) return Arr[0].value
-  if (Arr.findIndex((e) => e.per === 100) !== -1)return Arr.find((e) => e.per === 0)
-  if (Arr.reduce((pre, curr) => curr.per + pre, 0) > 100) return null
+  if (Arr.findIndex((e) => e.per === 100) !== -1) return Arr.find((e) => e.per === 0)
+  if (Arr.length === 0 || Arr.reduce((pre, curr) => curr.per + pre, 0) > 100) return null
   const trl = (n: number): number => n.toString().replace('.', '').length
   const sum = (i: number): number => Arr.reduce((pre, curr, I) => (I > i ? pre : pre + curr.per), 0)
   const max = Number(`1${'0'.repeat(trl(Arr.sort((a, b) => trl(b.per) - trl(a.per))[0].per))}`)
@@ -9,11 +9,12 @@ function random(Arr: Array<{ value: any; per: number }>): any {
   const asdf = (n: number): number => n.toString().includes('.') ? Number(n.toString().replace('.', '')) : Number(n.toString() + '0'.repeat(max.toString().slice(3).length))
   const PN = max > 100
   let index = -1
-  Arr.forEach((e, i) => {
+  for (const [i, e] of Arr.entries()) {
     const pre = asdf(sum(i - 1))
-    if (i === 0 && e.per <= ran) return (index = i)
-    else if (i === Arr.length - 1 && pre <= ran) return (index = i)
-    else if (pre <= ran && asdf(sum(i)) > ran) return (index = i)
-  })
+    if ((i === 0 && e.per <= ran) || (i === Arr.length - 1 && pre <= ran) || pre <= ran && asdf(sum(i)) > ran) {
+      index = i
+      break
+    }
+  }
   return Arr[index].value
 }
